9d605941625d81bf1d96fe0b29dd1329af3e8fe8
James Peters <jamespeters92@gmail.com>
2025-02-11 22:12:59 +0000

    BTD-20: Formatting change to branch name check git hook

--- hooks/git-hooks.js
+++ hooks/git-hooks.js
@@ -5,18 +5,14 @@ const branchName = shell.exec("git rev-parse --abbrev-ref HEAD", {silent: true})
 
 export function branchNameCheck() {
   if(!/^BTD-[0-9]+$/.test(branchName)) {
-    const errorMsg = `
-      Branch name does adhere to BTD ticket.
+    const errorMsg = `Branch name does adhere to BTD ticket.
       Your branch name is "${branchName}"
-      The branch name must be in the following format: "BTD-[NUMBER]".
-    `
+      The branch name must be in the following format: "BTD-[NUMBER]".`
     console.log(chalk.bgRgb(75, 0, 0).white.bold(errorMsg))
     process.exit(1)
   }
   else {
-    const successMsg = `
-      Branch name adheres to repo policy.
-    `
+    const successMsg = `Branch name adheres to repo policy.`
     console.log(chalk.bgRgb(0, 75, 0).white.bold(successMsg))
   }
 }
